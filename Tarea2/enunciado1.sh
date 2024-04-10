ddb8f08dc312f32749f920b7534c4840b78b54b3
StevenAOP <stevenaop2001@gmail.com>
2024-04-10 09:13:44 -0600

    Sugunda parte: Se implemetó la etapa que busca la información del proceso y la presenta al usuario

--- Tarea2/enunciado1.sh
+++ Tarea2/enunciado1.sh
@@ -13,3 +13,24 @@ if ! ps -p "$pid" > /dev/null 2>&1; then
     exit 1
 fi
 
+# Obteniendo información del ID con el comando ps
+nombre_proceso=$(ps -p "$pid" -o comm=)
+id_proceso="$1"
+parent_id=$(ps -p "$pid" -o ppid=)
+usuario_propietario=$(ps -p "$pid" -o user=)
+cpu_porcentaje=$(ps -p "$pid" -o %cpu=)
+mem_porcentaje=$(ps -p "$pid" -o %mem=)
+estado=$(ps -p "$pid" -o state=)
+# Se utiliza sudo para que pueda acceder al usuario root (superusuario)
+path_ejecutable=$(sudo readlink /proc/"$pid"/exe)
+
+# Mostrando la información al usuario
+echo "Información del proceso con ID $id_proceso:"
+echo "a) Nombre del proceso: $nombre_proceso"
+echo "b) ID del proceso: $id_proceso"
+echo "c) Parent process ID: $parent_id"
+echo "d) Usuario propietario: $usuario_propietario"
+echo "e) Porcentaje de uso de CPU: $cpu_porcentaje%"
+echo "f) Consumo de memoria: $mem_porcentaje%"
+echo "g) Estado: $estado"
+echo "h) Path del ejecutable: $path_ejecutable"
