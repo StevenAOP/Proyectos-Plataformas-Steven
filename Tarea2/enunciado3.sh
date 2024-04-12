77cfb8d20abd735b2c4ff7f905c6b8871434d5fa
StevenAOP <stevenaop2001@gmail.com>
2024-04-12 00:10:28 -0600

    Etapa de graficación corregida, script funcionando correctamente

--- Tarea2/enunciado3.sh
+++ Tarea2/enunciado3.sh
@@ -16,29 +16,30 @@ fi
 LOGFILE="proceso.log"
 
 # Agregando titulos a los datos por almacenar
-echo "Time CPU Memory" > $LOGFILE
+echo "Fecha-Hora CPU Memoria" > $LOGFILE
 
 # Ejecutando el binario (El ejecutable) en segundo plano
 "$1" &
 PID=$!
 
+
 # Función para obtener y registrar el uso de CPU y memoria
-function log_usage {
+function monitoreo {
     while kill -0 $PID 2>/dev/null; do
         # Capturando el %CPU y %memoria sin encabezados
         USAGE=$(ps -p $PID -o %cpu,%mem --no-headers)
         # Agregando una marca de tiempo
-        DATE=$(date +%s)
+        DATE=$(date +"%Y-%m-%d %H:%M:%S")
         # Escribiendo la información en el archivo .log
         echo "$DATE $USAGE" >> $LOGFILE
         # Reguistrando los valores cada 0.1 segundo
-        sleep 0.1 
+        sleep 0.5
     done
 }
 
 # Llamando a la función en segundo plano para el monitoreo y
 # agregar los datos
-log_usage &
+monitoreo &
 
 # Esperarando a que el proceso termine
 wait $PID
@@ -47,9 +48,9 @@ wait $PID
 # Graficar los resultados con gnuplot
 gnuplot -persist <<-EOFMarker
     set title "Consumo de CPU y Memoria"
-    set xlabel "Tiempo (s)"
+    set xlabel "Segundo en el que fue el proceso"
     set ylabel "Porcentaje"
-    set timefmt "%s"
+    set timefmt "%Y-%m-%d %H:%M:%S"
     set format x "%S"
     set xdata time
     plot "$LOGFILE" using 1:2 title 'CPU' with lines, \
