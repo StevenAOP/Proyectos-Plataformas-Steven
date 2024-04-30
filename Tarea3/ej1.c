b6bd991614f6793c1925abd13fe0764fe234087a
StevenAOP <stevenaop2001@gmail.com>
2024-04-29 22:49:35 -0600

    Se hace un cambio en el nombre del archivo. Además se implenta el código que imprime los primeros 100 números triangulares, donde se separan por comas y se imprimen de 5 en 5

---
+++ Tarea3/ej1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(){
+    // Declarando variables
+    int i;
+    int num_triangular;
+    for(i = 0; i < 100; i++){
+        num_triangular= i * (i + 1) / 2;
+        printf("%d", num_triangular);
+
+        // Coloca las comas a los número para distinguir bien cada número
+        if(i < 99){
+            printf(",");
+        }
+
+        // Imprime una nueva línea cada 5 números para dar un orden
+        if ((i + 1) % 5 == 0) {
+            printf("\n");
+        }
+    }
+    return 0;
+}
