a33fdb6628be7158eff8d2f2710c595f3c819e3d
StevenAOP <stevenaop2001@gmail.com>
2024-05-07 01:13:01 -0600

    Se agregan varios comentarios y se cambia la forma de solicitud del número al usuario, ahora se solicita mediante un oración

--- Tarea3/ej2.c
+++ Tarea3/ej2.c
@@ -4,16 +4,19 @@
 
 int factorial(int n){
     int i = 1;
+    // Lógica: Se mutiplica por n*(n-1)*(n-2)*...*(n-n+1)
     while(n > 1){
         i = i * n;
-        n = n - 1; // Corrección
+        n = n - 1; // Corrección (n se define antes)
     }
     return i;
 }
 
-int main(int arge, char *argv[]){
-    int num = atoi(argv[1]); // Se agregó
+int main(){
+    int num; // Declarando num
+    printf("Ingrese el número para calcular el factorial: ");
+    scanf("%d", &num); // Leyendo lo ingresado por el usuario
     int fac = factorial(num); // Corrección
-    printf("El factorial de %d es %d\n", num, fac); // Modificacción
+    printf("El factorial de %d es: %d\n", num, fac); // Modificacción
     return 0;
 }
