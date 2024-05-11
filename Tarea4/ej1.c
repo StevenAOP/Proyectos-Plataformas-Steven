fa84947e3efe7bb6718404649fc0117a0575fbfe
StevenAOP <stevenaop2001@gmail.com>
2024-05-11 17:40:41 -0600

    Se implemanta la función por busqueda lineal a un dato ingresado por el usuario para el arreglo dado

--- Tarea4/ej1.c
+++ Tarea4/ej1.c
@@ -2,10 +2,12 @@
 
 // Declaración de la funciones
 int solicitud();
+int busqueda_lineal(int x, int arreglo[]);
 
 int main(){
     int num = solicitud();
-    printf("%d \n", num);
+    int numeros[] = {2, 4, 6, 23, 56, 79};
+    busqueda_lineal(num, numeros);
     return 0;
 }
 
@@ -19,7 +21,26 @@ int main(){
 */
 int solicitud() {
     int n;
-    printf("Ingrese un número:");
+    printf("Ingrese un número: ");
     scanf("%d", &n); // Leyendo
     return n;
+}
+
+
+/*
+ * Esta función busca un número dentro de un arreglo dado.
+ * @param x El número a buscar
+ * @param arreglo[] El arreglo a manipular
+ * @return Cero para salir
+*/
+int busqueda_lineal(int x, int arreglo[]){
+    int len = 6;
+    for (int i = 0; i < len; i++){
+        if (x == arreglo[i]){
+            printf("El número %d se encuentra en la posición %d del arreglo. \n", arreglo[i], i);
+            return 0;
+        } 
+    }
+    printf("El número %d no se encuentra en el arreglo. \n", x);
+    return 0;
 }
