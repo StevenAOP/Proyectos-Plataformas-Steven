57c721c21746133d4323b78032b8a972c1f5e583
StevenAOP <stevenaop2001@gmail.com>
2024-05-15 23:58:07 -0600

    Se implementa la función que busca el valor mínimo y máximo de una función usando punteros

--- Tarea4/ej2.c
+++ Tarea4/ej2.c
@@ -4,6 +4,8 @@
 // Declaración de funciones
 int minimo(int arreglo[], int tamano);
 int maximo(int arreglo[], int tamano);
+void encontrarMinMax(int *arreglo, int * minimo, int * maximo, int longitud);
+
 
 int main(){
     int numeros[] = {21, 24, 65, 3, 56, 12, 35, 15};
@@ -13,7 +15,18 @@ int main(){
     printf("El número mínimo del arreglo es: %d \n", min);
 
     int max = maximo(numeros, tamano);
-    printf("El número máximo del arreglo es: %d \n", max);
+    printf("El número máximo del arreglo es: %d \n \n", max);
+
+    // Definiendo los punteros para la función encontrarMinMax()
+    int min2;
+    int max2;
+    int * ptr_numeros = numeros;
+    int * ptr_minimo = &min2;
+    int * ptr_maximo = &max2;
+
+    printf("Usando punteros\n");
+    encontrarMinMax(ptr_numeros, ptr_minimo, ptr_maximo, tamano);
+    printf("El número mínimo y máximo del arreglo son %d y %d respectivamente.\n", min2, max2);
 
     return 0;
 }
@@ -56,4 +69,36 @@ int maximo(int arreglo[], int tamano){
         }
     }
     return max_num;
+}
+
+
+/*
+ * Esta función almacena en dos punteros los numeros menores 
+   y menores de un arreglo dado en otro puntero
+ * @param *arreglo: El puntero de un arreglo a manipular.
+ * @param *minimo:  El puntero donde se almacena el número mínimo del arreglo.
+ * @param *maximo:  El puntero donde se almacena el número máximo del arreglo.
+ * @param longitud: El tamaño del arreglo.
+*/
+void encontrarMinMax(int *arreglo, int * minimo, int * maximo, int longitud){
+
+    // Dando los primeros datos del arreglo a minimo y maximo
+    *minimo = *arreglo;
+    *maximo = *arreglo;
+
+    /* Se recorre todo el arreglo del puntero comparando con el número
+    almacenado y si este es menor se cambia */
+    for (int i = 1; i < longitud; i++){
+        if (*(arreglo + i) < *minimo){
+            *minimo = *(arreglo + i);
+        }
+    }
+
+    /* Se recorre todo el arreglo del puntero comparando con el número
+    almacenado y si este es mayor se cambia */
+    for (int i = 1; i < longitud; i++){
+        if (*(arreglo + i) > *maximo){
+            *maximo = *(arreglo + i);
+        }
+    }
 }
