6230935856ab0e89b5554238add9fccd499e5750
StevenAOP <stevenaop2001@gmail.com>
2024-05-15 22:19:08 -0600

    Se implementa la función que encuentra el número mayor del arreglo

--- Tarea4/ej2.c
+++ Tarea4/ej2.c
@@ -3,6 +3,7 @@
 
 // Declaración de funciones
 int minimo(int arreglo[], int tamano);
+int maximo(int arreglo[], int tamano);
 
 int main(){
     int numeros[] = {21, 24, 65, 3, 56, 12, 35, 15};
@@ -10,6 +11,9 @@ int main(){
 
     int min = minimo(numeros, tamano);
     printf("El número mínimo del arreglo es: %d \n", min);
+
+    int max = maximo(numeros, tamano);
+    printf("El número máximo del arreglo es: %d \n", max);
 
     return 0;
 }
@@ -33,4 +37,23 @@ int minimo(int arreglo[], int tamano){
         }
     }
     return min_num;
+}
+
+/*
+ * Esta función busca el número mayor de un arreglo.
+ * @param arreglo[]: El arreglo a manipular.
+ * @param tamano:    El tamaño del arreglo.
+ * @return max_num:  El número mínimo del arreglo.
+*/
+int maximo(int arreglo[], int tamano){
+    int max_num = arreglo[0];
+
+    /* Se recorre todo el arreglo comparando con el número
+    almacenado y si este es mayor se cambia */
+    for (int i = 1; i < tamano; i++){
+        if (arreglo[i] > max_num){
+            max_num = arreglo[i];
+        }
+    }
+    return max_num;
 }
